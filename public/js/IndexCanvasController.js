587a791cd9dea664ef10dae4102a72d230d66061
Nick Stone <njstone1983@gmail.com>
2020-03-30 22:35:47 -0600

    COVORD NYEINRETORE

--- public/js/IndexCanvasController.js
+++ public/js/IndexCanvasController.js
@@ -1,6 +1,109 @@
 function IndexCanvasController(stage)
 {
 
+  let audioStarted = false;
+  // create web audio api context
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+  // create Oscillator node
+  var oscillator = audioCtx.createOscillator();
+  oscillator.type = 'triangle';
+  oscillator.frequency.setValueAtTime(7, audioCtx.currentTime); // value in hertz
+
+  var distortion = audioCtx.createWaveShaper();
+
+  function makeDistortionCurve(amount) {
+    var k = typeof amount === 'number' ? amount : 50,
+      n_samples = 44100,
+      curve = new Float32Array(n_samples),
+      deg = Math.PI / 180,
+      i = 0,
+      x;
+    for ( ; i < n_samples; ++i ) {
+      x = i * 2 / n_samples - 1;
+      curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+    }
+    return curve;
+  };
+
+distortion.curve = makeDistortionCurve(10);
+distortion.oversample = '4x';
+
+let filter = audioCtx.createBiquadFilter();
+filter.frequency.value = 0;
+filter.type = 'lowshelf';
+
+let gainNode = audioCtx.createGain();
+let mixer = audioCtx.createChannelMerger();
+let delayMixer = audioCtx.createChannelMerger();
+
+let fTypes = ['highpass', 'bandpass', 'lowpass', 'peaking', 'notch', 'allpass', 'highshelf', 'lowshelf'];
+
+oscillator.connect(filter);
+filter.connect(distortion);
+distortion.connect(delayMixer);
+
+gainNode.connect(mixer);
+
+mixer.connect(audioCtx.destination);
+
+let delay1 = audioCtx.createDelay(10);
+delay1.connect(mixer);
+
+let delay2 = audioCtx.createDelay(10);
+delay2.connect(mixer);
+
+// gainNode.connect(delayMixer);
+
+delayMixer.connect(delay1);
+delayMixer.connect(delay2);
+// delayMixer.connect(gainNode);
+
+gainNode.connect(mixer);
+
+function playNote(step)
+{
+  let osc1 = audioCtx.createOscillator();
+  let gate1 = audioCtx.createGain();
+  osc1.frequency.value = 90;
+  osc1.type = 'triangle';
+  osc1.connect(gate1);
+  gate1.gain.value = 0.5;
+  gate1.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 3);
+  gate1.connect(delayMixer);
+
+  let osc2 = audioCtx.createOscillator();
+  let gate2 = audioCtx.createGain();
+  osc2.frequency.value = 45;
+  osc2.type = 'square';
+  osc2.connect(gate2);
+  gate2.gain.value = 0.0;
+  gate2.gain.linearRampToValueAtTime(0.25, audioCtx.currentTime + 1.5);
+  gate2.gain.linearRampToValueAtTime(0, audioCtx.currentTime + 2);
+  gate2.connect(delayMixer);
+  osc1.start();
+  osc1.stop(audioCtx.currentTime + 3);
+  osc2.start();
+  osc2.stop(audioCtx.currentTime + 3);
+
+  // let split =
+
+}
+
+
+
+  window.addEventListener("click", ()=>
+  {
+    // playNote();
+    if(audioStarted)
+      return;
+    audioStarted = true;
+    filter.frequency.linearRampToValueAtTime(40, audioCtx.currentTime + 10);
+    oscillator.start();
+
+  });
+
+
 
   const NUM_VIRUS_PARTS = 20,
         NUM_VIRUS_LEGS = 6,
@@ -89,13 +192,23 @@ function IndexCanvasController(stage)
 
   var text = new createjs.Text("PROJECT COVID", "50px Work Sans, Helvetica, Arial", "#fff");
   text.textBaseline = "alphabetic";
-  let bounds = text.getBounds()
+  let bounds = text.getBounds();
   text.x = window.innerWidth / 2 - bounds.width / 2;
   text.y = window.innerHeight / 2;
+
+
+  var text2 = new createjs.Text("Hobbies of the Apocalypse", "30px Work Sans, Helvetica, Arial", "#fff");
+  text2.textBaseline = "alphabetic";
+  bounds = text2.getBounds();
+  text2.x = window.innerWidth / 2 - bounds.width / 2;
+  // text2.y = window.innerHeight / 2;
+  text2.y = text.y + bounds.height * 1.5;
 
   this.update = ()=>
   {
 
+
+    gainNode.value = 0.0;
 
     if(Math.random() < 0.05)
     {
@@ -104,7 +217,14 @@ function IndexCanvasController(stage)
       noise.lineSync = 0;
       text.scaleX = text.scaleY = 1;
       noise.source = blur;
+      distortion.curve = makeDistortionCurve(100);
+      filter.frequency.linearRampToValueAtTime(10, audioCtx.currentTime + 0.25);
+      filter.Q.value = 0.1;
+      oscillator.type = 'sine';
+      oscillator.frequency.setValueAtTime(7, audioCtx.currentTime); // value in hertz
+      // gainNode.value = 0.2;
     }
+
 
     if(Math.random() < 0.005)
     {
@@ -112,28 +232,54 @@ function IndexCanvasController(stage)
       noise.verticalSync =  Math.random();
       blur.amount = Math.random() * 0.03;
       noise.scanlines =  Math.random() * 200;
-
+      oscillator.type = 'square';
+      // gainNode.gain.value = 0.2;
     }
-    if(Math.random() < 0.02)
+    if(Math.random() < 0.001)
     {
       noise.lineSync = 0;
       blur.amount = Math.random() * 0.01;
+      oscillator.type = 'square';
+      // gainNode.gain.value = 0.3;
+      delay1.delayTime = 0.6;
+      delay2.delayTime = 0.8;
     }
+    if(Math.random() < 0.001)
+    {
+      blur.amount = Math.random() * 0.02;
+      noise.lineSync = Math.random() * 0.2;
+      filter.frequency.value = Math.random() * 4000;
+      filter.Q.value = Math.random() * 10;
+      oscillator.type = 'sawtooth';
+      // gainNode.gain.value = 0.5;
+    }
+
+
+
+
     if(Math.random() < 0.02)
     {
-      blur.amount = Math.random() * 0.05;
-    }
-    if(Math.random() < 0.005)
-    {
+
       noise.distortion = Math.random() * 0.3;
       text.scaleX = text.scaleY = Math.random() * 2;
       noise.lineSync = Math.random() * 0.2;
       noise.source = edge;
+      oscillator.frequency.value = Math.random() * 80;
       noise.verticalSync = Math.random() * 200;
+      gainNode.gain.value = 0.5;
+      distortion.curve = makeDistortionCurve(Math.random() * 600);
+      delay1.delayTime.value = Math.random() * 0.1;
+      delay2.delayTime.value = Math.random() * 0.1;
     }
-    if(Math.random() < 0.005)
+    if(Math.random() < 0.002)
     {
       noise.distortion = Math.random() * 0.3;
+      filter.Q.value = Math.random() * 10;
+      distortion.curve = makeDistortionCurve(Math.random() * 600);
+      filter.type = fTypes[Math.floor(Math.random())];
+      oscillator.frequency.value = Math.random() * 80;
+      filter.frequency.linearRampToValueAtTime(Math.random() * 4000, audioCtx.currentTime + 1);
+      gainNode.gain.value = 0.5;
     }
 
 
@@ -234,6 +380,7 @@ function IndexCanvasController(stage)
     }
 
     stage.addChild(text);
+    stage.addChild(text2);
 
     stage.update();
     source.update();
