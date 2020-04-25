c419a82b38a44cc0b8bad8a6c94b8a95fb26a348
Nick Stone <njstone1983@gmail.com>
2020-04-25 04:55:02 -0500

    more text stuff, can't get random array return to work

--- public/framework.php
+++ public/framework.php
@@ -13,21 +13,26 @@
   $title_prefix_count = count($title_prefix_array);
   $title_prefix_index = rand(0, $title_prefix_count - 1);
 
-  $title_suffix_array =  array("ly", "ify", "ful");
+  $title_suffix_array =  array("ly", "ify", "ful", "");
   $title_suffix_count = count($title_suffix_array);
   $title_suffix_index = rand(0, $title_suffix_count - 1);
 
-  $title = $title_prefix_array[$title_prefix_index] . $title_suffix_array[$title_suffix_index] . ".js";
 
   $subtitle_prefix_array =  array(
-    "Containerised", "Agile", "Flexible", "Dynamic",
-    "Lightning-fast", "Awesome", "Ninja", "Expressive",
-    "Lazy", "Incremental", "Platform-Agnostic", "Polymorphic", "Multi-Paradigm",
-    "Deep Learning", "Machine-Learning", "Powerful"
+    "Containerised", "Agile", "Flexible", "Dynamic", "Lightning-fast", "Awesome", "Ninja", "Expressive", "Lazy-loading", "Incremental",
+    "Deep learning", "Machine-learning", "Powerful", "Platform-agnostic", "Polymorphic", "Multi-paradigm", "Battle-hardened", "Field-tested"
   );
 
-  $subtitle_middle_array =  array("stylesheets", "syntax", "workflow", "components", "layouts", "APIs");
-  $subtitle_suffix_array =  array("at your fingertips", "out of the box", "at a click", "oven-ready");
+  $subtitle_middle_array =  array(
+    "stylesheets", "syntax", "workflow", "components", "layouts",
+    "APIs", "scheduling", "event models", "paradigms"
+  );
+  $subtitle_suffix_array =  array(
+    "at your fingertips", "out of the box",
+    "at a click", "oven-ready", "on-demand",
+    "done dynamically", "and then some",
+    "reimagined"
+  );
   $subtitle_prefix_count = count($subtitle_prefix_array);
   $subtitle_middle_count = count($subtitle_middle_array);
   $subtitle_suffix_count = count($subtitle_suffix_array);
@@ -35,39 +40,120 @@
   $subtitle_middle_index = rand(0, $subtitle_middle_count - 1);
   $subtitle_suffix_index = rand(0, $subtitle_suffix_count - 1);
 
-  $subtitle = $subtitle_prefix_array[$subtitle_prefix_index] . " " .
-              $subtitle_middle_array[$subtitle_middle_index] . ", " .
-              $subtitle_suffix_array[$subtitle_suffix_index];
-
-
   $subheadings =  array(
     "Containerised", "Agile", "Flexible",
     "Dynamic", "Lightning-fast", "Awesome",
     "Re-inventing the wheel",
     "Utterly Redundant", "Expressive",
     "Agnostic", "Polymorphic", "Multi-Paradigm",
-    "Plugin-Oriented", "Agile", "Does Nothing",
-    "Powerful"
+    "Plugin-Oriented", "Does Nothing",
+    "Powerful", "Battle-hardened", "Field-tested"
   );
-  $subheadings_count = count($subtitle_prefix_array);
+  $subheadings_count = count($subheadings);
+
+  $numbers = range(0, $subheadings_count - 1);
+  shuffle($numbers);
+
+
+  $summaries = array(
+    "Weighing in at just " + rand(0, 20) + " bytes, it's literally useless"
+  );
+
+
+  $title = $title_prefix_array[$title_prefix_index] . $title_suffix_array[$title_suffix_index] . ".js";
+
+  $subtitle = $subtitle_prefix_array[$subtitle_prefix_index] . " " .
+              $subtitle_middle_array[$subtitle_middle_index] . ", " .
+              $subtitle_suffix_array[$subtitle_suffix_index];
+
   $subheadings = array(
-    $subheadings[rand(0, $subheadings_count - 1)],
-    $subheadings[rand(0, $subheadings_count - 1)],
-    $subheadings[rand(0, $subheadings_count - 1)],
-    $subheadings[rand(0, $subheadings_count - 1)],
-    $subheadings[rand(0, $subheadings_count - 1)],
-    $subheadings[rand(0, $subheadings_count - 1)]
+    $subheadings[$numbers[0]],
+    $subheadings[$numbers[1]],
+    $subheadings[$numbers[2]],
+    $subheadings[$numbers[3]],
+    $subheadings[$numbers[4]],
+    $subheadings[$numbers[5]]
+  );
+
+  function rand_color() {
+    return sprintf('#%06X', mt_rand(0, 0xFFFFFF));
+  }
+
+  $col_pairs = array(
+    array("#222222", "#ffffff", "#666666", "#ff9900"),
+    array("#ffffff", "#222222", "#666666", "#0099ff"),
+    array("#fe43fd", "#2288aa", "#666666", "#ff33aa"),
+    array("#aa8822", "#28a284", "#666666", "#aaff33"),
+    array("#3dff67", "#fe43fd", "#666666", "#99aa55"),
+    array("#2288aa", "#21055a", "#666666", "#ffbbaa")
+  );
+
+  $col_index = rand(0, count($col_pairs) - 1);
+
+  $base_col_pair = $col_pairs[$col_index];
+
+  $bg = $base_col_pair[0];
+  $fg = $base_col_pair[1];
+
+  $bg1 = $base_col_pair[2];
+  $fg2 = $base_col_pair[3];
+
+  $n_return_phrases = 6;
+
+  $numbers = range(0, $n_return_phrases - 1);
+  shuffle($numbers);
+
+  $superlatives = array(
+    "awesome", "janky", "blazing-fast", "expressive", "mediocre", "redundant"
+  );
+
+  function get_superlative()
+  {
+    $result = $superlatives[0];
+    return $result;
+    // return
+    // return $superlatives[rand(0, count($superlatives) - 1)];
+  }
+
+  function return_phrase($index)
+  {
+    switch($index)
+    {
+      case 0:
+        return "A minimum-functionality philosophy leaves you free to write " . get_superlative() . " code!";
+      case 1:
+        return "It can't do much wrong if it doesn't do much at all.";
+      case 2:
+        return "It's a doddle to integrate, with our fire-and-forget approach: Simply import, then ignore!";
+      case 3:
+        return "Makes dependency-checking a breeze: it'll never have any, it'll never be one.";
+      case 4:
+        return "A modular architecture means that everything is organised and where you left it.";
+      case 5:
+        return "Weighing in at just " . rand(1, 20) . " bytes, it's literally useless.";
+
+    }
+  }
+
+  $phrases = array(
+    return_phrase($numbers[0]),
+    return_phrase($numbers[1]),
+    return_phrase($numbers[2]),
+    return_phrase($numbers[3]),
+    return_phrase($numbers[4]),
+    return_phrase($numbers[5])
   );
 
 
 
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Shit JS Framework</title>
+    <title><?php echo $title; ?></title>
     <style>
 
     body, html {
@@ -79,8 +165,8 @@
       border: 0;
       margin: 0;
       font-family: Montserrat;
-      background: #333;
-      color: #fff;
+      background: <?php echo $bg; ?>;
+      color: <?php echo $fg; ?>;
     }
     div {
       display: flex;
@@ -91,34 +177,13 @@
       font-weight: 100;
     }
 
-    .popup-container {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      justify-content: center;
-      align-items: center;
-      background-color: black;
-    }
-
-    .init-popup {
-      padding: 2rem;
-      background-color: white;
-      flex-direction: column;
-      color: black;
-      border-radius: 0.5rem;
-    }
-
-    .init-popup-text {
-      margin-bottom: 2rem;
-    }
-
     button {
       font-family: Montserrat;
-      background-color: #2196f3;
+      background-color: <?php echo $fg; ?>;
       border: none;
       outline: none;
       padding: 1rem;
-      color: white;
+      color: <?php echo $bg; ?>;
       font-size: 1rem;
       border-radius: 0.25rem;
       cursor: pointer;
@@ -137,11 +202,11 @@
 
     .three-columns {
         width: 100%;
-        background: #eee;
-        color: #222;
+        background: <?php echo $bg1; ?>;
+        color: <?php echo $fg2; ?>;
         padding-left: 10%;
         padding-right: 10%;
-        height: 15rem;
+        min-height: 15rem;
         /* justify-content: center; */
         /* align-items: center; */
         padding-top: 2rem;
@@ -154,7 +219,7 @@
       flex-direction: column;
     }
 
-    .column > h1 {
+    .column > h2 {
       font-size: 1.5rem;
       font-weight: 500;
       text-align: center;
@@ -164,6 +229,16 @@
       text-align: center;
       padding: 2rem;
       padding-top: 1rem;
+    }
+
+    @media (orientation: portrait) {
+      .three-columns {
+        flex-direction: column;
+        min-height: auto;
+      }
+      .main-screen {
+        overflow-y: scroll;
+      }
     }
 
     </style>
@@ -178,21 +253,21 @@
       <h4><?php echo $subtitle; ?></h4>
       <div class="three-columns">
         <div class="column">
-          <h1><?php echo $subheadings[0]; ?></h1>
+          <h2><?php echo $subheadings[0]; ?></h2>
           <section>
-            Knowing that this lightning-fast framework does fuck all leaves you free to write awesome code!
+            <?php echo $phrases[0]; ?>
           </section>
         </div>
         <div class="column">
-          <h1><?php echo $subheadings[1]; ?></h1>
+          <h2><?php echo $subheadings[1]; ?></h2>
           <section>
-            Organise your dependencies with our breathtaking tree-shaking technology
+            <?php echo $phrases[1]; ?>
           </section>
         </div>
         <div class="column">
-          <h1><?php echo $subheadings[2]; ?></h1>
+          <h2><?php echo $subheadings[2]; ?></h2>
           <section>
-            Modularised containers leave you free to talk shit on SO about other frameworks!
+            <?php echo $phrases[2]; ?>
           </section>
         </div>
       </div>
@@ -201,21 +276,21 @@
       <h4>Unlock the awesome</h4>
       <div class="three-columns">
         <div class="column">
-          <h1><?php echo $subheadings[3]; ?></h1>
+          <h2><?php echo $subheadings[3]; ?></h2>
           <section>
-            Weighing in at just 10 bytes, it's literally useless
+            <?php echo $phrases[3]; ?>
           </section>
         </div>
         <div class="column">
-          <h1><?php echo $subheadings[4]; ?></h1>
+          <h2><?php echo $subheadings[4]; ?></h2>
           <section>
-            You won't want to integrate anything else with this shit, but it is possible
+            <?php echo $phrases[4]; ?>
           </section>
         </div>
         <div class="column">
-          <h1><?php echo $subheadings[5]; ?></h1>
+          <h2><?php echo $subheadings[5]; ?></h2>
           <section>
-            It can't break if it never worked - simples!
+            <?php echo $phrases[5]; ?>
           </section>
         </div>
       </div>
@@ -230,7 +305,7 @@
       {
         let split = window.location.href.split("/");
         split.splice(split.length - 1, 1);
-        window.location.href = split.join("/") + "/" + Math.round(Math.random()*999999999);
+        window.location.href = split.join("/") + "/" + Math.round(Math.random()*999999999999);
       }
     </script>
   </body>
